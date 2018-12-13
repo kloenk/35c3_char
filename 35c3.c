9d2a714e84614ef033fef7fd60e8b1dd1e6c7fd0
Finn Behrens <finn@kloenk.de>
2018-12-13 19:49:25 +0100

    add udev rule

--- 35c3.c
+++ 35c3.c
@@ -11,34 +11,30 @@
 static dev_t first;
 static struct cdev c_dev;
 static struct class *c1;
-#define lenght 6
-static char c[lenght] = "35c3\n";
+#define lenght 4
+static char c[lenght] = "35c3";
 
 static int my_open(struct inode *i, struct file *f) {
-	printk(KERN_INFO "Kloenk: open()\n");
+	printk(KERN_DEBUG "35c3: open()\n");
 	return 0;
 }
 
 static int my_close(struct inode *i, struct file *f) {
-	printk(KERN_INFO "Kloenk: close()\n");
+	printk(KERN_DEBUG "35c3: close()\n");
 	return 0;
 }
 
 static ssize_t my_read(struct file *f, char __user *buf, size_t len, loff_t *off) {
-	//if (*off == 0) {
-		if (copy_to_user(buf, &c, lenght) != 0)
-			return -EFAULT;
-		else {
-			(*off)++;
-			return 3;
-		}
-	//} else {
-	//	return 0;
-	//}
+	if (copy_to_user(buf, &c, lenght) != 0)
+		return -EFAULT;
+	else {
+		(*off)++;
+		return lenght;
+	}
 }
 
 static ssize_t my_write(struct file *f, const char __user *buf, size_t len, loff_t *off) {
-	printk(KERN_INFO "Kloenk: write()\n");
+	printk(KERN_INFO "35c3: write()\n");
 	if (copy_from_user(&c, buf + len - lenght, lenght) != 0)
 		return -EFAULT;
 	else 
@@ -53,12 +49,12 @@ static struct file_operations pugs_fops = {
 	.write = my_write
 };
 
-static int __init ofd_init(void) {	// constructor
+static int __init c3_init(void) {	// constructor
 	int ret;
 	struct device *dev_ret;
 
-	printk(KERN_INFO "Kloenk: registered");
-	if ((ret = alloc_chrdev_region(&first, 0, 1, "Kloenk")) < 0) {
+	printk(KERN_INFO "35c3: registered");
+	if ((ret = alloc_chrdev_region(&first, 0, 1, "35c3")) < 0) {
 		return ret;
 	}
 
@@ -83,16 +79,16 @@ static int __init ofd_init(void) {	// constructor
 	return 0;
 }
 
-static void __exit ofd_exit(void) {	// destructor
+static void __exit c3_exit(void) {	// destructor
 	cdev_del(&c_dev);
 	device_destroy(c1, first);
 	class_destroy(c1);
 	unregister_chrdev_region(first, 1);
-	printk(KERN_INFO "Kloenk: unregisterd");
+	printk(KERN_INFO "35c3: unregisterd");
 }
 
-module_init(ofd_init);
-module_exit(ofd_exit);
+module_init(c3_init);
+module_exit(c3_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Kloenk <kloenk@kloenk.de>");
