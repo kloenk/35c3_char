e3fdbdf272fbf49395a80b9605408f5bc9278ccf
Kloenk <35c3@kloenk.de>
2018-12-17 16:07:36 +0100

    add second char dev that counts the time until 35c3

--- 35c3.c
+++ 35c3.c
@@ -2,6 +2,7 @@
 
 static dev_t first;
 static struct cdev c_dev;
+static struct cdev c_dev2;
 static struct class *c1;
 static char c[length] = "35c3·";
 
@@ -40,6 +41,97 @@ static struct file_operations pugs_fops = {
 	.write = my_write
 };
 
+static void daysTo(char *buf, time_t ns) {
+	int years, days, hours, minuts;
+	bool used = false;
+
+	years = ns / yearNS;
+	if (years >= 1) {
+		ns = ns - (years * yearNS);
+		sprintf(buf, "%d years, ", years); // create better , 
+		used = true;
+	} else { years = 0; }
+
+	days = ns / dayNS;
+	if (days >= 2) {
+		ns = ns - (days * dayNS);
+		if (used) {
+			sprintf(buf, "%s%d days, ", buf, days);
+		} else {
+			sprintf(buf, "%d days, ", days);
+		}
+		used = true;
+	} else { days = 0; }
+
+	hours = ns / hourNS;
+	if (hours >= 2) {
+		ns = ns - (hours * hourNS);
+		if (used) {
+			sprintf(buf, "%s%d hours, ", buf, hours);
+		} else {
+			sprintf(buf, "%d hours, ", hours);
+		}
+		used = true;
+	} else { hours = 0; }
+
+	minuts = ns / minutNS;
+	if (minuts >= 5) {
+		ns = ns - (minuts * minutNS);
+		if (used) {
+			sprintf(buf, "%s%d minuts, ", buf, minuts);
+		} else {
+			sprintf(buf, "%d minuts, ", minuts);
+		}
+		used = true;
+	} else { minuts = 0; }
+
+	if (used) {
+		sprintf(buf, "%s and %ld seconds\n", buf, ns);
+	} else {
+		sprintf(buf, "%ld seconds to go\n", ns);
+	}
+
+	// create char array
+}
+
+static ssize_t read_days_to(struct file *f, char __user *buf, size_t len, loff_t *off) {
+
+	if (*off == 0) {
+		struct timespec ts;
+		time_t deltaTime;
+
+		getnstimeofday(&ts);
+
+		if (ts.tv_sec < congressNS) {
+			deltaTime = congressNS - ts.tv_sec;
+		} else {
+			deltaTime = ts.tv_sec - congressEndNS;
+		}
+
+		char buffer[200];
+		daysTo(buffer, deltaTime);
+		printk(KERN_INFO "35C3Count: time: %s", buffer);
+
+
+		if (copy_to_user(buf, &buffer, strlen(buffer)) != 0)
+			return -EFAULT;
+		else {
+			(*off)++;
+			return strlen(buffer);
+		}
+		(*off)++;
+	} else {
+		return 0;
+	}
+}
+
+static struct file_operations count_fops = {
+	.owner   = THIS_MODULE,
+	.open    = my_open,
+	.release = my_close,
+	.read    = read_days_to
+};
+
 static int __init c3_init(void) {	// constructor
 	int ret;
 	struct device *dev_ret;
@@ -60,19 +152,42 @@ static int __init c3_init(void) {	// constructor
 		return PTR_ERR(dev_ret);
 	}
 
+	if (IS_ERR(dev_ret = device_create(c1, NULL, first + 1, NULL, "35c3Count"))) {
+		device_destroy(c1, first);
+		class_destroy(c1);
+		unregister_chrdev_region(first, charDevCount);
+		return PTR_ERR(dev_ret);
+	}
+
+	printk(KERN_INFO "DEVICES CREATED\n");
+
 	cdev_init(&c_dev, &pugs_fops);
 	if ((ret = cdev_add(&c_dev, first, 1)) < 0) {
 		device_destroy(c1, first);
+		device_destroy(c1, first + 1);
 		class_destroy(c1);
 		unregister_chrdev_region(first, charDevCount);
 		return ret;
 	}
+
+	cdev_init(&c_dev2, &count_fops);
+	if ((ret = cdev_add(&c_dev2, first +1 , 1)) < 0) {
+		cdev_del(&c_dev);
+		device_destroy(c1, first);
+		device_destroy(c1, first + 1);
+		class_destroy(c1);
+		unregister_chrdev_region(first, charDevCount);
+		return ret;
+	}
+
 	return 0;
 }
 
 static void __exit c3_exit(void) {	// destructor
 	cdev_del(&c_dev);
+	cdev_del(&c_dev2);
 	device_destroy(c1, first);
+	device_destroy(c1, first + 1);
 	class_destroy(c1);
 	unregister_chrdev_region(first, charDevCount);
 	printk(KERN_INFO "35c3: unregisterd\n");
--- 35c3.h
+++ 35c3.h
@@ -9,7 +9,16 @@
 #include <linux/device.h>
 #include <linux/cdev.h>
 #include <linux/uaccess.h>
+#include <linux/time.h>
 
 
 #define length 6	// length of char buffer
 #define charDevCount 2	// numbers of char devices
+
+#define congressNS	1545904800	// 12/27/18 10:00:00 AM UTC
+#define congressEndNS	1545904800	// 12/27/18 10:00:00 AM UTC
+
+#define yearNS  31536000
+#define dayNS	86400
+#define hourNS	3600
+#define minutNS 60
